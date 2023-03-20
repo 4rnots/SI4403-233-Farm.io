473b4940743b1123b0c68cd412a0bff05825bc43
Izurohmanq <marqozur@gmail.com>
2023-03-20 21:18:13 +0700

    GuruTernak admin FE

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuruController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\RegistController;
 
 /*
@@ -29,3 +31,24 @@ Route::get('/register', [RegistController::class, 'index']);
 
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/guruternak/login', [GuruController::class, 'loginGuru']);
+Route::post('/guruternak/login', [LoginController::class, 'authenticate']);
+
+Route::get('/guruternak/addclass', [GuruController::class, 'addclass']);
+Route::post('/guruternak/addclass', [CourseController::class, 'store']);
+
+Route::get('/guruternak/inbox', [GuruController::class, 'myinbox']);
+Route::put('/guruternak/inbox/{order}', [GuruController::class, 'update_status']);
+
+Route::get('/guruternak/setting', [GuruController::class, 'setting']);
+Route::put('/guruternak/setting/{user}', [GuruController::class, 'update']);
+
+Route::get('/guruternak/myclass', [CourseController::class, 'displayclass']);
+Route::delete('/guruternak/myclass/{course}', [CourseController::class, 'destroy']);
+
+Route::get('/guruternak/editclass', [CourseController::class, 'editclass']);
+Route::put('/guruternak/editclass/{course}', [CourseController::class, 'update']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
+
